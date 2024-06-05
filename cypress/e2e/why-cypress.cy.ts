043b4ef454863271f5f21a9457778a8dcfc2a4ac
savio <saviopf22@gmail.com>
2024-06-05 12:33:22 -0300

    type text in input with cypress

--- cypress/e2e/why-cypress.cy.ts
+++ cypress/e2e/why-cypress.cy.ts
@@ -1,5 +1,21 @@
 describe("tests in cypress playground", () => {
   it("cy type", () => {
     cy.visit("https://example.cypress.io");
+
+    cy.contains("type").click();
+
+    cy.get("#email1").focus();
+
+    cy.wait(2000);
+
+    cy.get("#email1").type("test@gmail.com");
+
+    cy.get("#email1").should("have.value", "test@gmail.com");
+
+    // generate error (input disabled)
+    // cy.get(":nth-child(2) > .form-control").type("test");
+
+    // ignore errors
+    cy.get(":nth-child(2) > .form-control").type("test", { force: true });
   });
 });
